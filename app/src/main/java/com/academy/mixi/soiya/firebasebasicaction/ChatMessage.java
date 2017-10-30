f9def7c66841bcf88aefd0f73f20836e0baea107
naohiro <k14053kk@aitech.ac.jp>
2017-10-30 14:22:53 +0900

    様々なデータの保存方法

--- app/src/main/java/com/academy/mixi/soiya/firebasebasicaction/ChatMessage.java
+++ app/src/main/java/com/academy/mixi/soiya/firebasebasicaction/ChatMessage.java
@@ -4,4 +4,10 @@ public class ChatMessage {
     public String body;
     public String sender;
     public long timestamp;
+
+    public ChatMessage(String body, String sender, long timestamp) {
+        this.body = body;
+        this.sender = sender;
+        this.timestamp = timestamp;
+    }
 }
--- app/src/main/java/com/academy/mixi/soiya/firebasebasicaction/MainActivity.java
+++ app/src/main/java/com/academy/mixi/soiya/firebasebasicaction/MainActivity.java
@@ -4,20 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseReference myRef;
     FirebaseDatabase database;
-    ValueEventListener vel;
-    Query orderByChildQuery;
-    Query orderByKeyQuery;
     static final String TAG = "Firebase";
 
     @Override
@@ -30,53 +26,50 @@ public class MainActivity extends AppCompatActivity {
         // jsonのトップレベルにある"messages"というノードを参照してます
         myRef = database.getReference("/messages");
 
-        // 変更が起こるとノードの要素を全部読み込む
-        final int BASIC = 1;
-        vel= new ValueEventListener() {
 
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                Log.d(TAG, "onDataChange ok");
-                // getChildren()メソッドで各要素をコレクションとして取り出す
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    ChatMessage chatMessage = dataSnapshot.getValue(ChatMessage.class);
-                    String sender = chatMessage.sender;
-                    String body = chatMessage.body;
-                    long timestamp = chatMessage.timestamp;
-                    Log.d("Firebase", String.format("onChildAdded, sender:%s, body:%s, timestamp:%d", sender, body, timestamp));
-                }
-            }
+        int mode = 4;
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // 読み込みに失敗したら呼び出される
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        };
-
-        // timestampを基準にソートしてくれた結果を吐く (昇順)
-        final int ORDER_BY_CHILD = 2;
-        orderByChildQuery = myRef.orderByChild("timestamp");
-
-        // 子要素のkey名でソートする (昇順)
-        // 今回参照されるのは 01, 02, 03...の部分でもともと昇順なので結果は変わらない
-        final int ORDER_BY_KEY = 3;
-        orderByKeyQuery = myRef.orderByKey();
-
-
-
-        int mode = BASIC;
-
-        switch (mode){
-            case BASIC:
-                myRef.addValueEventListener(vel);
+        switch (mode)
+        {
+            // デフォな追加方法
+            case 1:
+                myRef.child("01").setValue(new ChatMessage("デフォな追加方法で追加", "俺", System.currentTimeMillis()));
                 break;
-            case ORDER_BY_CHILD:
-                orderByChildQuery.addValueEventListener(vel);
-                break;
-            case ORDER_BY_KEY:
-                orderByKeyQuery.addValueEventListener(vel);
-                break;
+
+            // 完了コールバック
+            case 2:
+                myRef.child("02").setValue(new ChatMessage("完了コールバック付きで追加", "俺", System.currentTimeMillis()),
+                        new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                if(databaseError != null){
+                                    Log.d(TAG, "書き込みに失敗‥ :"+databaseError.getMessage(), databaseError.toException());
+                                }
+                                else{
+                                    Log.d(TAG, "書き込みに成功したよ！");
+                                }
+                            }
+                        });
+
+            // 一部だけ更新の場合 (今回は"body"だけを更新している)
+            case 3:
+                Map<String, Object> sender = new HashMap<>();
+                sender.put("body", "一部だけ更新した");
+                myRef.child("01").updateChildren(sender);
+
+            // myRef.child("指定のkey")だとコンフリクト起こした際上書きになるのでそれを防ぐ
+            // 指定のkeyの代わりに適当な文字列が入る
+            case 4:
+                myRef.push().setValue(new ChatMessage("コンフリクトを起こさない投稿", "admin", System.currentTimeMillis()));
+
+            // 以下のような使い方もできる
+            case 5:
+                String key = myRef.push().getKey();
+                Log.d(TAG,"key: "+key);
+                myRef.child(key).setValue(new ChatMessage("事前にkeyを把握してコンフリクトを起こさない投稿", "admin", System.currentTimeMillis()));
+
+            // runTransaction()ってのもあるけど挫折
+            // https://firebase.google.com/docs/database/android/read-and-write#updating_or_deleting_data#データの更新または削除
         }
 
     }
